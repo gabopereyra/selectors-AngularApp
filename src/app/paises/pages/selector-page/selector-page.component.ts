591a1efcfcda8e1ac6f1a8ae9b61f28ecd47f53b
gabopereyra <gabrielp1689@gmail.com>
2023-02-15 21:29:55 -0300

    Add array in paisesService with regions to use in region selector

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -11,15 +12,19 @@ export class SelectorPageComponent implements OnInit {
     region: ['', Validators.required],
   })
 
+  regiones : string[] = [];
+
   constructor(
     private fb : FormBuilder,
+    private paisesService : PaisesService
   ) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
   }
 
   guardar(){
-    
+
   }
 
 }
